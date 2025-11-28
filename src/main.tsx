357720663d9629a68d3746e68a401bd948704079
DirTy2304 <hugo.lambert23@sfr.fr>
2025-11-28 16:33:23 +0100

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+// src/main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { HashRouter } from "react-router-dom"; // ← ajouter
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>  {/* ← wrappe ton App */}
+    <App />
+  </HashRouter>
+);
