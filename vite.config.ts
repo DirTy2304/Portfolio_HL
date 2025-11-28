02fd0e5293333add5948fe4a03707ff314804ead
DirTy2304 <hugo.lambert23@sfr.fr>
2025-11-28 15:38:45 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio_HL/",   // ← AJOUT ICI
+
   server: {
     host: "::",
     port: 8080,
@@ -16,26 +18,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
