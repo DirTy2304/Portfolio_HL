67f9aedee0c20400f6991f6011cfdfeabcef9ec7
Hugo Lambert <160122575+DirTy2304@users.noreply.github.com>
2025-11-29 13:03:41 +0100

    UPDATE1

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
-// src/main.tsx
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom"; // ← ajouter
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>  {/* ← wrappe ton App */}
-    <App />
-  </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio_HL/",   // ← AJOUT ICI
+  // ⚠️ IMPORTANT pour GitHub Pages
+  // Mets ici EXACTEMENT le nom de ton repo GitHub
+  base: "/Portfolio_HL/",
 
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
